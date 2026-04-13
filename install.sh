582a1f85cb82ee44e639a24c3d8e3c6cecec133e
cairoGarcia <bromacaga@gmail.com>
2026-04-13 15:52:25 -0300

    Add install/uninstall script, media files and src/ directory for better project structure

---
+++ install.sh
@@ -0,0 +1,4 @@
+# Copies and gives executable permission
+cp $PWD/src/waypoint* $HOME/.local/bin/
+
+chmod +x $HOME/.local/bin/waypoint.lua $HOME/.local/bin/waypoint
---
+++ uninstall.sh
@@ -0,0 +1,2 @@
+# Removes ~/.local/bin/waypoint*
+rm $HOME/.local/bin/waypoint $HOME/.local/bin/waypoint.lua
